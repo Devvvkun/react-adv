d4d4b8fb71077e2294c978f7b042e74eb52bb7dd
Devvvkun <himanshusinghsvmsln@gmail.com>
2026-09-20 20:12:10 +0530

    Fixed Nav styling with nested routing

--- React_AdvanceRouting/Nested Routing/src/Navbar.jsx
+++ React_AdvanceRouting/Nested Routing/src/Navbar.jsx
@@ -3,10 +3,12 @@ const Navbar = () => {
   return (
     <div className='flex justify-between  p-6 h-14  items-center bg-blue-400 text-gray font-semibold'>
         <h3>NavBar</h3>
+        <div className='w-[30%] flex gap-9'>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/contact'>Contact</Link>
         <Link to='/products'>Products</Link>
+    </div>
     </div>
   )
 }
